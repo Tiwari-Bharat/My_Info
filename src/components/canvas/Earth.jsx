20522beeb80bd09d116557f93fea95091b5947c9
Tiwari-Bharat <tiwaribharat5321@gmail.com>
2025-10-22 17:26:10 +0530

    first commit

---
+++ src/components/canvas/Earth.jsx
@@ -0,0 +1,51 @@
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { isWebGLAvailable } from "../../utils/webglDetection";
+
+const Earth = () => {
+  const earth = useGLTF("./planet/scene.gltf");
+  return (
+    <primitive object={earth.scene} scale={3} position-y={0} rotation-y={0} />
+  );
+};
+
+const EarthCanvas = () => {
+  const [webglSupported, setWebglSupported] = useState(true);
+
+  useEffect(() => {
+    setWebglSupported(isWebGLAvailable());
+  }, []);
+
+  if (!webglSupported) {
+    return null;
+  }
+
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+    >
+      <Suspense fallback={null}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
+        <Preload all />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default EarthCanvas;
