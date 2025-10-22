9e1f77810cfef18961b2747cc0511df9d9d69c0a
Tiwari-Bharat <tiwaribharat5321@gmail.com>
2025-10-22 11:20:05 +0530

    Initial project setup

---
+++ src/utils/webglDetection.js
@@ -0,0 +1,9 @@
+export const isWebGLAvailable = () => {
+  try {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+    return !!context;
+  } catch (e) {
+    return false;
+  }
+};
