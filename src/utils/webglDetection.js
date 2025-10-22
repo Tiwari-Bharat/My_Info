70037a0896e887c1b0b0d7293e07272c95ce74ec
Tiwari-Bharat <tiwaribharat5321@gmail.com>
2025-10-22 16:49:01 +0530

    Revert "Initial project setup"
    This reverts commit 9e1f77810cfef18961b2747cc0511df9d9d69c0a.

--- src/utils/webglDetection.js
+++
@@ -1,9 +0,0 @@
-export const isWebGLAvailable = () => {
-  try {
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-    return !!context;
-  } catch (e) {
-    return false;
-  }
-};
